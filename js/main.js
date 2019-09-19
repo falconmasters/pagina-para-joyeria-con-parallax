06e534f941219fa7a785b83cce4974e3802647e8
Carlos Arturo <carlos@falconmasters.com>
2019-09-19 12:49:56 -0500

    Agregando archivos del proyecto

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const altura = document.body.scrollHeight - window.innerHeight;
+const fondo = document.getElementById('fondo');
+
+window.onscroll = () => {
+	const anchoFondo = (window.pageYOffset / altura) * 700;
+	if(anchoFondo <= 100){
+		fondo.style.width = anchoFondo + '%';
+	}
+}
